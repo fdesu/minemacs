44c604efbaa724ce27c3cc260a85c88aa4544e26
fdesu <merkel05@gmail.com>
2018-12-28 12:02:13 +0100

    Version control customzation

--- custom.el
+++ custom.el
@@ -8,10 +8,37 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+
+ '(custom-enabled-themes
+   (quote (deeper-blue)))
+ 
  '(package-selected-packages
    (quote
-    (groovy-mode tabbar projectile powerline pdf-tools paradox nyan-mode neotree meghanada markdown-mode helm-rtags helm-gtags helm google-c-style magit gitconfig-mode git-timemachine flycheck-rtags flycheck dashboard company-quickhelp company-c-headers company auto-complete)))
- '(paradox-github-token t))
+    (modern-cpp-font-lock
+     groovy-mode
+     tabbar
+     projectile
+     powerline
+     pdf-tools
+     paradox
+     nyan-mode
+     neotree
+     meghanada
+     markdown-mode
+     helm-rtags helm-gtags helm
+     google-c-style
+     magit gitconfig-mode git-timemachine
+     flycheck-rtags flycheck
+     dashboard company-quickhelp company-c-headers company
+     auto-complete)))
+
+ '(paradox-github-token t)) ;;; remove if github token needed
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
